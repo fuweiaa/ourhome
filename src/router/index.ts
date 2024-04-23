76892f0c734f459deb95b3da85405f8befd8fdc5
shaohsuangyue <1615447617@qq.com>
2024-04-23 14:58:56 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,49 @@
+/*
+ * @Author: fuwei
+ * @Date: 2024-04-22 23:47:20
+ * @LastEditors: fuweiaa
+ * @LastEditTime: 2024-04-23 10:44:19
+ * @FilePath: \bigevent-vue3\src\router\index.ts
+ * @Description:
+ * Copyright (c) 2024 by VGE, All Rights Reserved.
+ */
+/*
+ * @Author: YangYuhan 861896230u@qq.com
+ * @Date: 2023-03-04 09:40:31
+ * @LastEditors: 枫林残忆 2997534654@qq.com
+ * @LastEditTime: 2024-04-09 08:55:41
+ * @FilePath: \Geology-V3\src\router\index.ts
+ * @Description: 路由
+ */
+
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Root",
+    component: () => import("../views/login.vue"),
+  },
+  {
+    path: "/home",
+    component: () => import("@/views/home.vue"),
+    meta: {
+      keepAlive: false,
+    },
+  },
+  {
+    path: "/social",
+    component: () => import("../views/socialfriends/index.vue"),
+    meta: {
+      keepAlive: false,
+    },
+  },
+  // 其他路由...
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="container">
+        首页登录界面
+    </div>
+    <router-link to="home">点击跳转到首页</router-link>
+    <router-link to="social">点击跳转到首页</router-link>
+</template>
+
+<script setup lang="ts">
+// import { onMounted } from "vue";
+// onMounted(() => {
+//     console.log('hello world');
+    
+// }),
+</script>
+
+<style lang="scss" scoped>
+.container{
+    width: 500px;
+    height: 500px;
+    background-color: aqua;
+}
+</style>
