325e5081345de5e059986d62e7e7cb1f23550f54
fuweiaa <2567873016@qq.com>
2024-04-23 14:35:02 +0800

    hell

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <!--
  * @Author: fuwei
  * @Date: 2024-04-21 23:47:28
- * @LastEditors: fuwei
- * @LastEditTime: 2024-04-23 00:39:13
- * @FilePath: \bigevent-vite\src\App.vue
+ * @LastEditors: fuweiaa
+ * @LastEditTime: 2024-04-23 14:34:50
+ * @FilePath: \bigevent-vue3\src\App.vue
  * @Description: 
  * Copyright (c) 2024 by VGE, All Rights Reserved. 
 -->
 <template>
   <div>
         <router-view />
+        <div>daasdaasd</div>
   </div>
 
 </template>
