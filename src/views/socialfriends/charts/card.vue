8b18e1664e5ed1f3770a34673b4f9df95e93f99f
shaohsuangyue <1615447617@qq.com>
2024-04-25 16:57:28 +0800

    玥玥设计的朋友圈

--- src/views/socialfriends/charts/card.vue
+++ src/views/socialfriends/charts/card.vue
@@ -1,8 +1,10 @@
 <template>
-<div class="container">
-    <div class="userInfo">用户头像</div>
-    <div class="socialInfo">朋友圈资料</div>
-</div>
+  <div>
+    <p class="head">we are family</p>
+    <el-scrollbar height="100vh">
+      <p v-for="item in 20" :key="item" class="scrollbar-demo-item">第{{ item }}条朋友圈</p>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,14 +12,32 @@
 </script>
 
 <style lang="scss" scoped>
-.container {
-    position: relative;
-    display: flex;
-    margin: auto;
-    top: 20px;
-    width: 80vw;
-    height: 20vh;
-    background-color: aquamarine;
+.head {
+  font-family: Arial, sans-serif;
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+  background-color: #bae6f8;
+  line-height: 2.2;
+  background-image: url("src/image/1.png"), url('src/image/2.png');
+  background-position: left center, right center;
+  background-repeat: no-repeat, no-repeat;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-size: 200px auto, 200px auto;
 }
 
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+  width: auto;
+}
 </style>
