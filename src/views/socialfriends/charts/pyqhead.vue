a4afcd854c618f07d4cd2ef352de4cbb60976129
fuweiaa <2567873016@qq.com>
2024-04-29 17:40:47 +0800

    没什么好说的

--- src/views/socialfriends/charts/pyqhead.vue
+++ src/views/socialfriends/charts/pyqhead.vue
@@ -1,7 +1,16 @@
+<!--
+ * @Author: fuweiaa
+ * @Date: 2024-04-28 16:50:03
+ * @LastEditors: fuweiaa
+ * @LastEditTime: 2024-04-29 17:39:48
+ * @FilePath: \bigevent-vue3\src\views\socialfriends\charts\pyqhead.vue
+ * @Description: 
+ * Copyright (c) 2024 by VGE, All Rights Reserved. 
+-->
 <template>
-    <div>
-      <p class="head">we are family</p>
-    </div>
+  <div>
+    <p class="head">we are family</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,7 +26,7 @@
   text-align: center;
   background-color: #bae6f8;
   line-height: 2.2;
-  background-image: url("src/image/1.png"), url('src/image/2.png');
+  background-image: url("/assets/image/1.png"), url('/assets/image/2.png');
   background-position: left center, right center;
   background-repeat: no-repeat, no-repeat;
   background-size: 200px auto, 200px auto;
